e8d1169fd86cdd7e39045ada69d068544c974a39
Alex Pakhunov <alexeypa@gmail.com>
2022-05-08 11:17:37 -0700

    Demonstrate SIGBUS on an unaligned memory access.

--- rabbit.cc
+++ rabbit.cc
@@ -1,11 +1,26 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <random>
 
 #include "AlignmentGuard.h"
 
 int main() {
     AlignmentGuard aligment_guard;
 
-    std::cout << "Hello world\n";
+    std::random_device rd;
+    std::uniform_int_distribution<unsigned> dist;
+
+    /*
+     * Write at an unaligned address.
+     */
+    std::array<char, 4096> buf;
+    *reinterpret_cast<unsigned*>(&buf[1]) = dist(rd);
+
+    /*
+     * Make sure writing to the buffer above is not optimized out.
+     */
+    std::cout << std::accumulate(buf.begin(), buf.end(), unsigned{});
 
     return 0;
 }
